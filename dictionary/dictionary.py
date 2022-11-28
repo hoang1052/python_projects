415c5bb83b581b802960fd979d019f22afd41373
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-11-29 00:07:22 +0700

    modified:   dictionary/dictionary.py     upgraded to get the close matches

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,5 +1,5 @@
 import json
-
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(word):
@@ -10,8 +10,18 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
+    elif len(get_close_matches((word), data.keys())) > 0 :
+        print("did you mean %s instead " % get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes or press n for no")
+        if decide == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif decide == "n":
+            return " You have entered wrong word please check it again "
+        else:
+            return " You have entered wrong input please enter just y or n "
+        
     else:
-        print("You have entered wrong word please check it again")
+        print(" You have entered wrong word please check it again ")
         
 word = input("Enter word you want to search for: ")
 output = translate(word)
