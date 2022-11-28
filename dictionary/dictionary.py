37b779e5c1d4546cbde7415a9593525f3e7327df
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-11-28 21:32:15 +0700

    modified:   dictionary/dictionary.py

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -2,4 +2,9 @@ import json
 
 data = json.load(open("data.json"))
 
-print(data["smog"])  
+def translate(word):
+    if word in data:
+        return data[word]
+word = input("Enter word you want to search for: ")
+output = translate(word)
+print(output)
