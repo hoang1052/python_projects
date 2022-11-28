3c2e29bd8eb9d322e005554edf9933a37520815d
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-11-29 01:09:45 +0700

    modified:   hangman_game/hangman.py     Create the world's list

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,5 +1,12 @@
+import random
+
+def hangman():
+    word = random.choice(["pugger", "tiger", "thor", "superman", "pokemon", "earth"])
+    return print(word)
+    
 name = input("Enter your name: ")
 print("Welcome", name)
 print("=====================")
 print("Try to guess the word in less than 10 attempts")
+hangman()
 print()
