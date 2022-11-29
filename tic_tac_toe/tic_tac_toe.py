52f70f8f3bef8f4e30b20cc92668905bc5bd0b94
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-11-30 00:51:19 +0700

    modified:   tic_tac_toe/tic_tac_toe.py     Add isWinner: tic_tac_toe

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -23,3 +23,13 @@ def isBoardFull(board):
         return False
     else :
         return True
+
+def isWinner(b,l):#board, letter
+    return ((b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l))
