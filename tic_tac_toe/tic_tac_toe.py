ffddd364b8ba04f3aa55a0e6e90cf732352c42d0
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-11-30 01:04:50 +0700

    modified:   tic_tac_toe/tic_tac_toe.py     Add playerMove

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -33,3 +33,21 @@ def isWinner(b,l):#board, letter
     (b[3] == l and b[6] == l and b[9] == l) or
     (b[1] == l and b[5] == l and b[9] == l) or
     (b[3] == l and b[5] == l and b[7] == l))
+    
+def playerMove():
+    run = True
+    while run:
+        move = input("Please select a position to enter the X between 1 to 9 ")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X' , move)
+                else:
+                    print('Sorry, this space is occupied')
+            else:
+                print('Please type a number between 1 and 9 ')
+
+        except:
+            print('Please type a number')
