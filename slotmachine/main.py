f03d2813615a56210df5f4f58fc2e30c804b522a
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-12-01 21:24:29 +0700

    modified:   slotmachine/main.py     Add function slotmachine_spin

--- slotmachine/main.py
+++ slotmachine/main.py
@@ -1,8 +1,44 @@
+import random
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
 
+sympol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, sympols):
+    all_sympols = []
+    for sympol, sympol_count in sympols.items():       #items in dictionary = key + value
+        for _ in range(sympol_count):
+            all_sympols.append(sympol)
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_sympols = all_sympols[:]                #make a copy of all_sympols
+        for _ in range(rows):
+            value = random.choice(current_sympols)
+            current_sympols.remove(value)
+            column.append(value)                        #add value to column
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end = " | ")
+            else:
+                print(column[row], end = "")
+        print()
+                
+        
 def deposit():
     while True:
         amount = input("What do you want to deposit? $")
@@ -43,6 +79,7 @@ def get_numbet_of_lines():
     return lines
 
 
+
 def main():
     balance = deposit()
     lines = get_numbet_of_lines()
@@ -59,3 +96,6 @@ def main():
     #print(balance, lines)
 
 main()
+
+slots = get_slot_machine_spin(ROWS, COLS, sympol_count)
+print_slot_machine(slots)
