6505913bd8a58dd8b51fe1253f18eee60d11d2d2
hoang1052 <huynhhuuhoang1052@gmail.com>
2022-12-01 19:26:04 +0700

    modified:   slotmachine/main.py     Add function number of line

--- slotmachine/main.py
+++ slotmachine/main.py
@@ -1,10 +1,14 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 
 def deposit():
     while True:
         amount = input("What do you want to deposit? $")
         if amount.isdigit():
             amount = int(amount)
-            if amount > 0
+            if amount > 0:
                 break
             else:
                 print("Amount must be greater than 0. ")
@@ -12,3 +16,25 @@ def deposit():
             print("Please enter a number ")
     return amount
 
+
+def get_numbet_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines. ")
+        else:
+            print("Please enter a number ")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_numbet_of_lines()
+    print(balance, lines)
+
+main()
